402c65ec6bd46c2819385c8cdb6d07349b123fd1
alex <alex@ritzycapital.com>
2019-01-17 15:55:53 -0600

    server on port

--- src/config/server.js
+++ src/config/server.js
@@ -3,22 +3,23 @@ const morgan = require('morgan');
 const express = require('express');
 const routes = require('../routers');
 const cors = require("cors");
+const { server_port } = require('./keys');
 
 module.exports = app => {
     //setings 
-    app.set('port' , process.env.PORT || 4000);
+    app.set('port', process.env.PORT || server_port.port);
 
     //midlewares
     app.use(morgan('dev'));
     app.use(cors())
-    app.use(express.urlencoded({extended : false}));
+    app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
     //routes
     routes(app);
 
     //static file
-    app.use('/public' , express.static(path.join(__dirname , '../public')));
-    
+    app.use('/public', express.static(path.join(__dirname, '../public')));
+
     return app;
 }
